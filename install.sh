335fc8fc0235c41a3e10e2b1b6d7bc336d3df6c5
Hussam Saadeh <hmxy87@gmail.com>
2026-06-17 19:00:09 +0300

    Fix bug in reload waybar keybind and make left click is the default click for alt-format in waybar modules and move install.sh script to home directory

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 cp -r ../fonts/* ~/.local/share/fonts/
 fc-cache -fv 
+
 echo "Installing fonts completed"
-cp -r ../hypr ~/.config/.
-cp -r ../kitty ~/.config/.
-cp -r ../waybar ~/.config/.
-cp -r ../wlogout ~/.config/.
+
+cp -r hypr ~/.config/.
+cp -r kitty ~/.config/.
+cp -r waybar ~/.config/.
+cp -r wlogout ~/.config/.
 
 echo "Installing config files completed"
