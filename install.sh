e4a7def050d434821ba3b26d943607e2da824546
Hussam Saadeh <hmxy87@gmail.com>
2026-08-07 23:12:35 +0300

    Enhance wallpaper-selctore.sh script (wallpaper-selector.sh now can scan subdirectory of the main wallpaper directory) and adding custome rofi theme for wallpaper-selector

--- install.sh
+++ install.sh
@@ -8,6 +8,8 @@ cp -r wlogout ~/.config/.
 echo "Root premetion is needed for installing rofi theme to this folder [/usr/share/rofi/themes]"
 sudo cp -r rofi/themes/* /usr/share/rofi/themes/
 cp -r rofi/config.rasi ~/.config/rofi/
+cp -r rofi/rofi-wallpaper-selector-theme.rasi ~/.config/rofi/
+
 cp -r scripts ~/.local/share/
 chmod +x ~/.local/share/scripts/*
 echo "Installing config files completed"
--- scripts/wallpaper-selector.sh
+++ scripts/wallpaper-selector.sh
@@ -1,55 +1,28 @@
 #!/usr/bin/env bash
 
-WALL_DIR="$HOME/Pictures/Wallpapers"
+WALL_DIR="$HOME/Pictures/wallpapers"
+ROFI_THEME="$HOME/.config/rofi/rofi-wallpaper-selector-theme.rasi"
 
 if [ ! -d "$WALL_DIR" ]; then
     notify-send "Error" "Wallpaper directory not found."
     exit 1
 fi
 
-# Turquoise Theme Layout
-ROFI_STYLE="
-configuration {
-    show-icons: true;
-}
-window {
-    width: 65%;
-    location: south;
-    anchor: south;
-    y-offset: -50px;
-    border-radius: 12px;
-    border: 2px;
-    border-color: #14B8A6;
-    background-color: #0d1517cc;
-}
-element {
-    orientation: vertical;
-    padding: 15px;
-    border-radius: 8px;
-}
-element-icon {
-    size: 160px;
-    horizontal-align: 0.5;
-}
-element-text {
-    horizontal-align: 0.5;
-    color: #E2F5F3;
-}
-"
-
 # Construct a list where each item passes its text name AND its absolute path as the icon asset
 MENU_ITEMS=""
-while IFS= read -r file; do
+while IFS= read -r filepath; do
+    file=$(basename filepath)
     if [[ "$file" =~ \.(png|jpg|jpeg|webp|gif)$ ]]; then
         # Format: DisplayName\0icon\x1f/Absolute/Path/To/Image
         MENU_ITEMS+="${file}\0icon\x1f${WALL_DIR}/${file}\n"
     fi
-done < <(ls "$WALL_DIR")
+done < <( find "$WALL_DIR" -type f)
 
 # Launch Rofi with the formatted list strings
-SELECTED=$(echo -e "$MENU_ITEMS" | rofi -dmenu -theme-str "$ROFI_STYLE" -p "Select Wallpaper")
+SELECTED=$(echo -e "$MENU_ITEMS" | rofi -show-icons -theme "$ROFI_THEME" -dmenu -p "Select Wallpaper")
 
 if [ -n "$SELECTED" ]; then
-    awww img "$WALL_DIR/$SELECTED" --transition-type "outer" --transition-step 90
+    filepath=$(find "$WALL_DIR" -type f -name "$SELECTED" | head -n 1)
+    awww img "$filepath" --transition-type "outer" --transition-step 90
     notify-send "Wallpaper Changed" "Applied: $SELECTED"
 fi
