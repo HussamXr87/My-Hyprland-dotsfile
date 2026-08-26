80c604b6983cb7e2829aec61480989cc80744cfb
Hussam Saadeh <hmxy87@gmail.com>
2026-08-26 20:49:33 +0300

    Fix simple error in keymapings.lua

--- hypr/hyprland/keybindings.lua
+++ hypr/hyprland/keybindings.lua
@@ -18,7 +18,7 @@ hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + SPACE", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + SHIFT + R", hl.dsp.exec_cmd("rofi-theme-selector"))
-hl.bind(mainMod .. " +  SHIFT B", hl.dsp.exec_cmd("killall waybar || waybar"))
+hl.bind(mainMod .. " +  SHIFT + B", hl.dsp.exec_cmd("killall waybar || waybar"))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(browser))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
