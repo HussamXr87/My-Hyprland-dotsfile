4aec2b680e50c7e141cfea132cf60a14c29856a3
Hussam Saadeh <hmxy87@gmail.com>
2026-09-23 16:01:38 +0300

    Organize the repo by creating config folder

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
 cp -r fonts/* ~/.local/share/fonts/
 fc-cache -f
 echo "Installing fonts completed"
-cp -r hypr ~/.config/.
-cp -r kitty ~/.config/.
-cp -r waybar ~/.config/.
-cp -r wlogout ~/.config/.
-echo "Root premetion is needed for installing rofi theme to this folder [/usr/share/rofi/themes]"
-sudo cp -r rofi/themes/* /usr/share/rofi/themes/
-cp -r rofi/config.rasi ~/.config/rofi/
-cp -r rofi/rofi-wallpaper-selector-theme.rasi ~/.config/rofi/
+
+cp -rf config/* ~/.config/
+echo "Installing config files completed"
 
 cp -r scripts ~/.local/share/
 chmod +x ~/.local/share/scripts/*
-echo "Installing config files completed"
+echo "Installing Scripts completed"
