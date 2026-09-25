123f5ddb39c7b63bd697257eec51457ea827e58f
Hussam Saadeh <hmxy87@gmail.com>
2026-09-26 00:01:47 +0300

    balabal

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
-# cp -r fonts/* ~/.local/share/fonts/
-# fc-cache -f
-# echo "Installing fonts completed"
+#!/usr/bash
+
+echo "Do you want to install Nerd Font and Font Awesome for icons [y/n]?"
+
+read UserAnswer
+
+case "${UserAnswer^^}" in 
+
+    Y|YES)
+        cp -r fonts/* ~/.local/share/fonts/
+        fc-cache -f
+        echo "Installing fonts completed"
+    ;;
+    N|NO)
+        echo "OK"
+
+
 
 cp -rf config/* ~/.config/
 echo "Installing config files completed"
