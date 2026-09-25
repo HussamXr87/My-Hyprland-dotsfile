8c8dc7d54fa12640883ad2f7ddf8416d3aab9398
Hussam Saadeh <hmxy87@gmail.com>
2026-09-25 21:11:25 +0300

    perf: add more icon in waybar backlight module and fix bug in brightness control shortcut

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-cp -r fonts/* ~/.local/share/fonts/
-fc-cache -f
-echo "Installing fonts completed"
+# cp -r fonts/* ~/.local/share/fonts/
+# fc-cache -f
+# echo "Installing fonts completed"
 
 cp -rf config/* ~/.config/
 echo "Installing config files completed"
